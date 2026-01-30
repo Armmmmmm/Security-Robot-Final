a4692667620d0e5e7ccde2f6ea9d523918b4471a
Armmmmmm <patinya7883@gmail.com>
2026-01-30 10:53:35 +0700

    First clean commit: Robot source code and world

---
+++ src/mobile_robotics_update/launch/camera.launch.py
@@ -0,0 +1,17 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                'image_size': [640, 480],
+                'camera_frame_id': 'camera_link_optical',
+                'gamma': 100,
+                'pixel_format': 'YUYV'  # เปลี่ยนจาก MJPG เป็น YUYV
+            }]
+        )
+    ])
